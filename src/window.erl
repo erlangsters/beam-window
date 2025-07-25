75459b5c2636cb8d52085b1cebd24983413a8d1d
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2025-07-25 15:49:34 +0800

    Add placeholder files to sketch the API

---
+++ src/window.erl
@@ -0,0 +1,17 @@
+-module(window).
+-moduledoc """
+To be written.
+
+To be written.
+""".
+
+-export([foobar/0]).
+
+-doc """
+To be written.
+
+To be written.
+""".
+-spec foobar() -> integer().
+foobar() ->
+    42.
